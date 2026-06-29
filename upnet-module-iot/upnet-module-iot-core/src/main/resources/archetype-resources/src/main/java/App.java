a6c5ccbc9dd21a118afbe4db3e55d2a5ddf6c6ca
dexter <chenhonghu@upnet.com.cn>
2026-06-29 16:48:15 +0800

    project(iot): 初始化iot项目工程文件

---
+++ upnet-module-iot/upnet-module-iot-core/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.upnet.iot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
