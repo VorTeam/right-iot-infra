22f69e9fdcf79bac04597efef74e458bb6eeb141
dexter <chenhonghu@upnet.com.cn>
2026-06-30 15:40:30 +0800

    refactor(iot): 移除默认控制器并清理启动类注释
    - 删除 DefaultController.java 默认控制器
    - 精简 UpnetServerApplication 启动类注释文档，统一代码风格

--- upnet-server/src/main/java/com/upnet/iot/server/UpnetServerApplication.java
+++ upnet-server/src/main/java/com/upnet/iot/server/UpnetServerApplication.java
@@ -5,11 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 项目的启动类
- *
- * 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
- * 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
- * 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
- *
  * @author 芋道源码
  */
 @SuppressWarnings("SpringComponentScan") // 忽略 IDEA 无法识别 ${upnet.info.base-package}
@@ -17,18 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UpnetServerApplication {
 
     public static void main(String[] args) {
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
-
         SpringApplication.run(UpnetServerApplication.class, args);
-//        new SpringApplicationBuilder(UpnetServerApplication.class)
-//                .applicationStartup(new BufferingApplicationStartup(20480))
-//                .run(args);
-
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
-        // 如果你碰到启动的问题，请认真阅读 https://doc.iocoder.cn/quick-start/ 文章
     }
 
 }
